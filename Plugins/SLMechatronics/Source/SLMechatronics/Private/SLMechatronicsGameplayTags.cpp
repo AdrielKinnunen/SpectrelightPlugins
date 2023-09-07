45f93d8501bbfcce99013ae1fd4a87e6e10bbe37
Adriel Kinnunen <111756925+AdrielKinnunen@users.noreply.github.com>
2023-09-07 01:46:55 -0400

    Forgot to delete these

--- Plugins/SLMechatronics/Source/SLMechatronics/Private/SLMechatronicsGameplayTags.cpp
+++
@@ -1,8 +0,0 @@
-﻿// Copyright Spectrelight Studios, LLC
-
-#include "SLMechatronicsGameplayTags.h"
-
-UE_DEFINE_GAMEPLAY_TAG(TAG_SLMechatronics_Domain_Signal, "SLMechatronics.Domain.Signal");
-UE_DEFINE_GAMEPLAY_TAG(TAG_SLMechatronics_Domain_Drivetrain, "SLMechatronics.Domain.Drivetrain");
-UE_DEFINE_GAMEPLAY_TAG(TAG_SLMechatronics_Domain_Electricity, "SLMechatronics.Domain.Electricity");
-UE_DEFINE_GAMEPLAY_TAG(TAG_SLMechatronics_Domain_Fluids, "SLMechatronics.Domain.Fluids");
