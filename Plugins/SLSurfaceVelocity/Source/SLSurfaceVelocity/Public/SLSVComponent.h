8507468cb11b0bcae17dc99a986d74f970bbd434
Adriel Kinnunen <111756925+AdrielKinnunen@users.noreply.github.com>
2025-07-03 00:29:15 -0400

    Added component and subsystem files

---
+++ Plugins/SLSurfaceVelocity/Source/SLSurfaceVelocity/Public/SLSVComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SLSVComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SLSURFACEVELOCITY_API USLSVComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USLSVComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
---
+++ Plugins/SLSurfaceVelocity/Source/SLSurfaceVelocity/Public/SLSVSubsystem.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "SLSVSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLSURFACEVELOCITY_API USLSVSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+};
